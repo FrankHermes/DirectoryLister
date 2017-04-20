e42b481d024d426d62afe7e15f180eba21c745ad
Frank Hermes <frank@ijsfontein.nl>
2017-04-20 13:11:21 +0200

    canon-updates theme with json info support

---
+++ resources/themes/canon-updates/default_footer.php
@@ -0,0 +1,5 @@
+<hr>
+
+<h6 class="footer" style="align:left;">
+    Powered by  <a href="http://www.directorylister.com">Directory Lister</a>
+</h6>
---
+++ resources/themes/canon-updates/default_header.php
@@ -0,0 +1,6 @@
+<img src="https://www.ijsfontein.nl/wp-content/themes/ijsfontein/img/ijsfontein-logo.png"/>
+<img src="https://www.openluchtmuseum.nl/sites/default/files/inline-images/logo_clean.png"/ style="
+	margin-top: -100px;
+	margin-bottom: -80px;
+"><br/>
+<h1>Canon van Nederland installers</h1>
